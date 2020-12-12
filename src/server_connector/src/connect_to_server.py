a870aca22d4ad22e0a030c372bc3f50213967ae7
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2020-12-11 23:31:14 -0800

    Added connect_to_server

--- src/server_connector/src/connect_to_server.py
+++ src/server_connector/src/connect_to_server.py
@@ -1,13 +1,17 @@
 import roslibpy
-import socket
 
 
-def get_ip():
-    try:
-        host_name = socket.gethostname()
-        return socket.gethostbyname(host_name)
-    except StandardError:
-        print('Unable to get device ip')
+def register_drone(client):
+    service = roslibpy.Service(client, '/isaacs_server/register_drone', 'isaacs_server/register_drone')
+    request = roslibpy.ServiceRequest({'drone_name': 'hexacopter', 'drone_type': 'MavrosDrone'})
+
+    result = service.call(request)
+    print('register_drone response: {}'.format(result))
 
 
-ip = get_ip()
+server_ip = '192.168.254.35'
+server_port = 9090
+server_connection = roslibpy.Ros(host=server_ip, port=server_port)
+server_connection.run(timeout=20)
+if server_connection.is_connected:
+    register_drone(server_connection)
